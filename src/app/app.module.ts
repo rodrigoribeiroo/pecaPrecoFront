61304442251a313a444165ff229102df377d3d41
rodrigoribeiroo <rodrigodeabreuribeiro@gmail.com>
2018-11-28 05:09:51 -0200

    pagina de historico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { BluetoothSerial } from ' @ ionic-native / bluetooth-serial ' ;
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 
 import { MyApp } from './app.component';
 
